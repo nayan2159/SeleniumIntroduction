eb0e8b13bcf591db20550d5a68a863cae18916a5
nayanproject-admin <nayanlambat12@gmail.com>
2026-08-01 08:09:28 +0530

    feat: automate login using dynamically retrieved password

--- src/Locators2.java
+++ src/Locators2.java
@@ -9,19 +9,43 @@ public class Locators2 {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-        String name="Rahul";
+		String name = "Rahul";
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\nayan\\OneDrive\\Documents\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		String password = getPassword(driver);
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys(name);
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.name("inputPassword")).sendKeys(password);
 		driver.findElement(By.className("signInBtn")).click();
 		Thread.sleep(2000);
 		System.out.println(driver.findElement(By.tagName("p")).getText());
-		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(),"You are successfully logged in.");
-		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(),"Hello " +name+",");
+		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(),
+				"Hello " + name + ",");
+		driver.findElement(By.xpath("//button[text()='Log Out']")).click();
+		driver.close();
+	}
+
+	public static String getPassword(WebDriver driver) throws InterruptedException {
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		String getText = driver.findElement(By.cssSelector("form p")).getText();
+
+		// Please use temporary password 'rahulshettyacademy' to Login.
+		String[] getpassWord1 = getText.split("'");
+		// getpassWord1[0] = "Please use temporary password"
+		// getpassWord1[1]="rahulshettyacademy' to Login."
+
+		String[] getpassWord2 = getpassWord1[1].split("'");
+		// getpassWord2[0]
+
+		String passWord = getpassWord2[0];
+		return passWord;
+
 	}
 
 }
