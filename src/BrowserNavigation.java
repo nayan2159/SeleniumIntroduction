08c26a4c6e80edc66b02dc016262ed019a78356b
nayanproject-admin <nayanlambat12@gmail.com>
2026-08-06 08:12:36 +0530

    feat: practice browser navigation with back and forward actions

---
+++ src/BrowserNavigation.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserNavigation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\nayan\\OneDrive\\Documents\\chromedriver-win64\\chromedriver.exe");	
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://google.com");
+		driver.navigate().to("https://rahulshettyacademy.com");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.quit();
+		
+		
+	
+	}
+
+}
